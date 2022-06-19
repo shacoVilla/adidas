334319f28b1102378d909ad92a2ca07eda005993
danielvillacres <danielvilla18@gmail.com>
2022-06-19 17:02:27 +0200

    Added unit test for the subscription application service

--- subscription/src/test/java/com/adidas/subscription/domain/service/DomainSubscriptionServiceTest.java
+++ subscription/src/test/java/com/adidas/subscription/domain/service/DomainSubscriptionServiceTest.java
@@ -1,7 +1,5 @@
 package com.adidas.subscription.domain.service;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
@@ -9,99 +7,102 @@ import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.adidas.subscription.application.request.SubscriptionRequest;
 import com.adidas.subscription.application.response.SubscriptionResponse;
-import com.adidas.subscription.application.rest.SubscriptionController;
 import com.adidas.subscription.domain.Subscription;
+import com.adidas.subscription.domain.repository.SubscriptionRepository;
+import com.adidas.subscription.infrastructure.repository.h2database.DatabaseSubscriptionRepository;
 import com.adidas.subscription.infrastructure.repository.h2database.H2DbSubscriptionRepository;
-import com.adidas.subscription.infrastructure.repository.h2database.SubscriptionEntity;
 import com.adidas.subscription.utils.Gender;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
-public class DomainSubscriptionServiceTest
-{
+@RunWith(MockitoJUnitRunner.class)
+public class DomainSubscriptionServiceTest {
 
 	@Autowired
-	private MockMvc mockMvc;
+	private H2DbSubscriptionRepository h2databaserepository;
 
-	@Autowired
-	private ObjectMapper mapper;
+	private SubscriptionRepository repo;
 
-	@Autowired
-	H2DbSubscriptionRepository repository;
+	@InjectMocks
+	private DomainSubscriptionService service;
 
-	/*@Test
-	void shouldCreateSubscription() throws Exception {
-		SubscriptionRequest subscriptionRequest = getSubscriptionRequest();
-
-		String subscriptionRequestString = mapper.writeValueAsString(subscriptionRequest);
-		
-		mockMvc = MockMvcBuilders.standaloneSetup(new SubscriptionController()).build();
-
-		mockMvc.perform(MockMvcRequestBuilders.post("/subscription").contentType(MediaType.APPLICATION_JSON_VALUE)
-				.content(subscriptionRequestString)).andExpect(status().isCreated());
-
-		Assertions.assertTrue(repository.count() == 1);
+	@BeforeEach
+	public void setUp() {
+		repo = new DatabaseSubscriptionRepository(h2databaserepository);
+		service = new DomainSubscriptionService(repo);
 	}
 
 	@Test
-	void shouldReturnAListOfSubscriptions() throws Exception {
+	void shouldCreateNewSubscription() {
+		service.createNewSubscription(getSubscriptionRequest());
 		
-		List<Subscription> listSubs = getSubscriptionsList();
-		
-		List<SubscriptionEntity> entities = listSubs.stream().map(s -> new SubscriptionEntity(s)).toList();
-		repository.saveAll(entities);
-		
-		mockMvc.perform(MockMvcRequestBuilders.post("/subscription/all")
-				.contentType(MediaType.APPLICATION_JSON_VALUE))
-		.andExpect(status().isOk());
-		
+		Assertions.assertTrue(h2databaserepository.count() == 1);
+	}
+	
+	@Test
+	void shouldCancelSubscription() {
+
+		SubscriptionResponse response = service.createNewSubscription(getSubscriptionRequest());
+
+		service.cancelExistingSubscription(response.getId());
+
+		Assertions.assertTrue(h2databaserepository.count() == 0);
 	}
 
-	private SubscriptionResponse toSubscriptionResponse(Subscription subscription) {
-		return SubscriptionResponse.builder().id(subscription.getId()).consent(subscription.getConsent())
-				.datebirth(subscription.getDatebirth()).email(subscription.getEmail()).gender(subscription.getGender())
-				.newsletterid(subscription.getNewsletterid()).id(subscription.getId()).build();	
+	@Test
+	void shouldGetSubscriptionDetails() {
+
+		SubscriptionResponse responseCreated = service.createNewSubscription(getSubscriptionRequest());
+
+		Long idSubscription = responseCreated.getId();
+
+		SubscriptionResponse responseDetails = service.getSubscriptionDetails(idSubscription);
+
+		Assertions.assertTrue(responseCreated.equals(responseDetails));
 	}
-	private List<Subscription> getSubscriptionsList(){
+
+
+	@Test
+	void shouldReturnAListOfSubscriptions() throws Exception {
+
+		List<Subscription> listSubs = getSubscriptionsList();
+
+		listSubs.stream().forEach(s -> repo.save(s));
+
+		List<SubscriptionResponse> result = service.getAllSubscriptions();
+
+		Assertions.assertTrue(result.size() == 2);
+	}
+
+	private List<Subscription> getSubscriptionsList() {
 		List<Subscription> list = new ArrayList<Subscription>();
-		
+
 		ZoneId defaultZoneId = ZoneId.systemDefault();
-		
-		Subscription subscription1 = Subscription.builder()
-				.firstname("dan1")
-				.consent(true)
+
+		Subscription subscription1 = Subscription.builder().firstname("dan1").consent(true)
 				.datebirth(Date.from(LocalDate.of(2000, 11, 21).atStartOfDay(defaultZoneId).toInstant()))
-				.email("email1")
-				.gender(Gender.FEMALE)
-				.newsletterid("sjchshdj")
-				.id(1L).build();
-		
-		Subscription subscription2 = Subscription.builder()
-				.firstname("dan2")
-				.consent(true)
+				.email("email1").gender(Gender.FEMALE).newsletterid("sjchshdj").id(1L).build();
+
+		Subscription subscription2 = Subscription.builder().firstname("dan2").consent(true)
 				.datebirth(Date.from(LocalDate.of(2000, 11, 21).atStartOfDay(defaultZoneId).toInstant()))
-				.email("email1")
-				.gender(Gender.FEMALE)
-				.newsletterid("sjchshdj")
-				.id(2L).build();
+				.email("email1").gender(Gender.FEMALE).newsletterid("sjchshdj").id(2L).build();
 		list.add(subscription1);
 		list.add(subscription2);
-		
+
 		return list;
 	}
 
@@ -111,5 +112,5 @@ public class DomainSubscriptionServiceTest
 		return SubscriptionRequest.builder().firstname("DanielV").gender(Gender.MALE)
 				.datebirth(Date.from(LocalDate.of(2000, 11, 21).atStartOfDay(defaultZoneId).toInstant())).consent(true)
 				.email("someth@email.com").newsletterid("1233433323").build();
-	}*/
+	}
 }
